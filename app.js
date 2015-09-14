94fdbf25b5a14d0c873ecbbb5d465ede42c531f3
Rimas Silkaitis <neovintage@gmail.com>
2015-09-14 14:38:16 -0700

    something that looks like an app

--- app.js
+++ app.js
@@ -1,50 +1,84 @@
-var express = require('express')
-  , http    = require('http')
-  , appfunc = require('./appfunc')
-  , debug   = require('debug')('twitter:listenter')
-  , app     = express()
-  , port    = appfunc.normalizePort(process.env.PORT || '3000')
-  , logger  = require('morgan')
-  , twitter = require('twitter');
+var express   = require('express')
+  , path      = require('path')
+  , http      = require('http')
+  , appfunc   = require('./appfunc')
+  , debug     = require('debug')
+  , app       = express()
+  , socket_io = require('socket.io')
+  , port      = appfunc.normalizePort(process.env.PORT || '3000')
+  , logger    = require('morgan')
+  , twitter   = require('twitter')
+  , pg        = require('pg')
+  , db        = new pg.Client(process.env.DATABASE_URL)
+  , moment    = require('moment');
 
 
+/*********************************************************
+ *
+ * Initial Setup
+ *
+ */
+var server = http.createServer(app)
+  , io     = socket_io(server);
+db.connect();
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+
+/*********************************************************
+ *
+ * App Routes and Logic
+ *
+ */
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('index');
+});
+
+io.on('connection', function(socket) {
+  console.log('Client Connected');
 });
 
 
-/*
+/*********************************************************
+ *
  *  Setup Twitter to start pulling tweets
  *
  */
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+var client = new twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-client.stream('statuses/filter', {track: ''}, function(stream) {
+client.stream('statuses/filter', {track: 'rimas dreamforce'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    var short_tweet = {
+      tweet_id: tweet.id_str,
+      username: tweet.user.name,
+      tweet: tweet.text,
+      profile_url: tweet.user.profile_image_url,
+      created_at: moment(tweet.timestamp_ms, 'x')
+    };
+    console.log(short_tweet);
+    io.sockets.emit('data',short_tweet);
+    //db.query("INSERT into tweets () values ()", );
   });
 });
 
-
-// catch 404 and forward to error handler
-//
+/************************************************************
+ *
+ *  Error handling and rest of the middle ware to get going
+ *
+ */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-/******************
- * error handlers
- ******************/
 // development error handler
 // will print stacktrace
 //
@@ -69,10 +103,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
-/**
- * start the webserver
+
+/*******************************************************
+ *
+ * Starting the Webserver
+ *
  */
-var server = http.createServer(app);
 server.listen(port);
 server.on('error', appfunc.onError);
 server.on('listening', function() {
---
+++ migration.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS tweets (
+  id           serial,
+  tweet_id     text,
+  username     text,
+  tweet        text,
+  profile_url  text,
+  created_at   timestamp without time zone
+);
+
+CREATE INDEX tweets_created_at ON tweets (created_at);
+CREATE INDEX tweets_id ON tweets (id);
