4e8ba982e8c311b650663263c0f750fa5d6e6952
Rimas Silkaitis <neovintage@gmail.com>
2015-12-03 03:20:49 -0800

    working version

--- consumer_cassie.js
+++ consumer_cassie.js
@@ -23,18 +23,35 @@ var cassandra     = require('cassandra-driver')
 var cassandra_uri  = process.env.CASSANDRA_URL.split(',')
   , contact_points = []
   , username       = ""
-  , password       = "";
+  , password       = ""
+  , keyspace       = "";
 
 for(var i = 0; i < cassandra_uri.length; i++) {
   var parsed_uri = url.parse(cassandra_uri[i]);
-  contact_points.push(parsed_uri.host);
+  contact_points.push(parsed_uri.hostname);
   var auth = parsed_uri.auth.split(':');
   username = auth[0];
   password = auth[1];
+  keyspace = parsed_uri.path.replace('/','');
 }
 
 var authProvider  = new cassandra.auth.PlainTextAuthProvider(username, password)
-  , client        = new cassandra.Client({ contactPoints: contact_points, authProvider: authProvider, keyspace: 'examples' });
+  , client        = new cassandra.Client({
+                      contactPoints: contact_points,
+                      authProvider: authProvider,
+                      keyspace: keyspace,
+                      sslOptions: {
+                        cert: process.env.CASSANDRA_TRUSTED_CERT,
+                        secureProtocol: 'TLSv1_method'
+                      }
+                    });
+
+var checkIt = function(err) {
+  if(err) {
+    client.shutdown();
+    return console.log(err);
+  }
+}
 
 //
 // The magic happens here
@@ -47,8 +64,7 @@ client.connect(function(err) {
 
   // Create the keyspace and table if it doesn't already exist
   //
-  //client.execute("CREATE KEYSPACE IF NOT EXISTS examples WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }");
-  client.execute("CREATE TABLE IF NOT EXISTS tweets (tweet_id, username text, screen_name text, tweet text, profile_url text, created_at timestamp, PRIMARY KEY (username, created_at))");
+  client.execute("CREATE TABLE IF NOT EXISTS tweets (tweet_id bigint, username text, screen_name text, tweet text, profile_url text, created_at timestamp, PRIMARY KEY (username, created_at))", checkIt);
 
   console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
   console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
@@ -59,10 +75,6 @@ client.connect(function(err) {
     var query    = "INSERT INTO examples.tweets (tweet_id, username, screen_name, tweet, profile_url, created_at) values (?, ?, ?, ?, ?, ?)";
     var new_date = moment(msg.created_at, 'x');
     var params   = [msg.tweet_id, msg.username, msg.screen_name, msg.tweet, msg.profile_url, new_date.format("YYYY-MM-DD HH:mm")];
-    client.execute(query, params, { prepare: true }, function(err, result) {
-      if(err) {
-        console.log(err);
-      }
-    });
+    client.execute(query, params, { prepare: true }, checkIt);
   });
 });
